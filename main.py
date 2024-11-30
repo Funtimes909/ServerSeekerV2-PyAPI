85b90c193959a2cbfca5d6930fda30fd571e0054
EngurRuzgar <info@nucceteere.xyz>
2024-11-30 10:51:20 +0000

    feat: add stats endpoint along with a license

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from typing import Union
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic import BaseModel
+from psycopg.rows import class_row
 import psycopg
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+conn = psycopg.connect("dbname=test user=fastapi password=nucceteere host=localhost")
+
+@app.get("/stats")
+def stats():
+    class Stats(BaseModel):
+        count: int
+    cur = conn.cursor(row_factory=class_row(Stats))
+    stats = cur.execute("SELECT COUNT(*) FROM servers").fetchone()
+    return {"ServerNumber":f"{stats.count}"}
