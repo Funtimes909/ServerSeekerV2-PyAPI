f2b6e6e95b211f7f9d153de2422ae5fe6da11561
EngurRuzgar <info@nucceteere.xyz>
2025-01-17 11:20:56 +0000

    docs: update stats documentation [ci skip]

--- utils/responses.py
+++ utils/responses.py
@@ -57,7 +57,29 @@ random = {
     {
      "content":
      {
-      "application/json"
+      "application/json":
+      {
+       "example":
+        {
+                "address": "1.1.1.1",
+                "port": 25565,
+                "version": "1.21.1",
+                "protocol": "767",
+                "software": "JAVA",
+                "motd": "A Minecraft Server",
+                "country": "US",
+                "asn": "AS24940 Hetzner Online GmbH",
+                "org": "Hetzner Online GmbH",
+                "firstseen": 1732377334,
+                "lastseen": 1732377334,
+                "hostname": "mail.funtimes909.xyz",
+                "whitelist": False,
+                "cracked": False,
+                "enforces_secure_chat": False,
+                "prevents_reports": False,
+                "icon": "data:image/png;base64,iVBORw0KGg... (base64 encoded image)"
+        }
+       }
      },
         "description": "Returns a random server"
     },
@@ -129,9 +151,30 @@ stats = {
       "application/json":
       {
         "example":
-       {
-        "servers": "250863"
-       }
+        {
+            "all": 417519,
+            "java": 149253,
+            "modded": {
+                "lexforge": 65946,
+                "neoforge": 16530
+            },
+            "plugin": {
+                "bukkit": 885,
+                "spigot": 16585,
+                "paper": 55814,
+                "purpur": 3807,
+                "pufferfish": 364
+            },
+            "multi_threaded": {
+                "folia": 165,
+                "lumina": 0
+            },
+            "proxies": {
+                "velocity": 6835,
+                "waterfall": 1411,
+                "bungeecord": 2820
+            }
+        }
       }
      }
      , "description": "Return Stats"
