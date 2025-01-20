9b9b4b37db8b321a70dce83f2c495ec4a3a0e212
EngurRuzgar <info@nucceteere.xyz>
2025-01-20 23:29:48 +0000

    chore: to be depracated default overridden

--- utils/database.py
+++ utils/database.py
@@ -6,4 +6,5 @@ pool = ConnectionPool(
     conninfo=f"dbname={const.DATABASE_NAME} user={const.DATABASE_USER} password={const.DATABASE_PASSWORD} host={const.DATABASE_HOST} port={const.DATABASE_PORT}",
     min_size=1,
     max_size=50,
+    open=True
 )
